34e2fef910bc92f0140240d7e2e4d3df2834cec1
Shea Rial <sheatrevor@gmail.com>
2011-09-21 11:31:31 -0400

    Changed the way this worked.

--- spellbook/diskspace-on-switches/execute.sh
+++ spellbook/diskspace-on-switches/execute.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Define the list of switch hostnames
-servers="switch8.mongoosemetrics.com \
-    switch9.mongoosemetrics.com \
-    switch10.mongoosemetrics.com \
-    switch11.mongoosemetrics.com \
-    switch12.mongoosemetrics.com \
-    switch13.mongoosemetrics.com \
-    switch14.mongoosemetrics.com \
-    switch15.mongoosemetrics.com \
-    switch16.mongoosemetrics.com"
+# Define the list of switch servers
+servers=`spellbook get-switch-servers`
 
 # Loop over all of the files in the current directory
-for hostname in $servers; do
+for server in $servers; do
+    # Display the current hostname
+    echo $server
     # Display the result of 'df -h' on the current host
-    ssh root@$hostname 'df -h'
+    ssh root@$server 'df -h'
 done
---
+++ spellbook/get-switch-servers/execute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Echo the list of switch server names
+echo "switch8.mongoosemetrics.com \
+    switch9.mongoosemetrics.com \
+    switch10.mongoosemetrics.com \
+    switch11.mongoosemetrics.com \
+    switch12.mongoosemetrics.com \
+    switch13.mongoosemetrics.com \
+    switch14.mongoosemetrics.com \
+    switch15.mongoosemetrics.com \
+    switch16.mongoosemetrics.com"
