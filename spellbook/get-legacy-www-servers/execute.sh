e7cb81c734b4284757f6ac7d32a0e67486145e49
Shea Rial <sheatrevor@gmail.com>
2012-01-26 16:44:16 -0500

    We know how to remove files now.

---
+++ spellbook/get-legacy-www-servers/execute.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Echo the list of www servers
+echo "www1.mongoosemetrics.com \
+    www2.mongoosemetrics.com"
---
+++ spellbook/remove-campaign-audio-file/execute.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# If no arguments were passed in
+if [ "$#" == "0" ]; then
+    # Display the usage instructions for this script, then stop
+    echo "Usage: spellbook remove-campaign-audio-file [file name]"
+    exit 1
+fi
+
+# Grab the name of the audio file we are looking for
+audio_file=$1
+
+# Grab the list of switch servers
+switch_servers=`spellbook get-switch-servers`
+
+# Loop over each of the switches
+for server in $switch_servers; do
+    # Log on and remove the audio file from the current switch
+    ssh root@$server 'rm -i /var/lib/asterisk/sounds/'$audio_file
+done
+
+# Grab the list of the legacy www servers
+www_servers=`spellbook get-legacy-www-servers`
+
+# Loop over each of the web servers
+for server in $www_servers; do
+    # Log on and remove the audio file from the www server
+    ssh root@$server 'rm -i /var/www/html/www.mongoosemetrics.com/sounds/'$audio_file
+done
