3439c3c11c13ea5dd09edfb792043352ce759b31
Shea Trevor <sheatrevor@gmail.com>
2012-08-14 18:02:55 -0400

    Updates from the upstream source.

--- spellbook/redis-vim/execute.sh
+++ spellbook/redis-vim/execute.sh
@@ -11,12 +11,14 @@ KEY=$1
 TMPFILE=$(mktemp /tmp/XXXXXXXXXX.js)
 
 redis-cli -h r1-dev get $1 >$TMPFILE
-sed -i -e 's/^"//g;s/"$//g;s/\\//g;s/{/{\n/g;s/}/\n}/g;s/,/,\n/g;' $TMPFILE
+sed -i -e 's/{/{\n/g;s/}/\n}/g;s/,/,\n/g;' $TMPFILE
 vim -c 'argdo execute "normal gg=G" | update' $TMPFILE
-sed -i -e 's/^[ \t]*//g;s/"/\\"/g' $TMPFILE
-VALUE=$( printf '"' ; cat $TMPFILE | tr -d "\n" ; printf '"' )
+sed -i -e 's/^[ \t]*//g;' $TMPFILE
+VALUE=$( cat $TMPFILE | tr -d "\n" )
 echo
-echo $VALUE
+# Bash automatically escapes data in an environment variable.
+# I output the sample to reflect how bash escapes it's data.
+echo $VALUE | sed -e 's/"/\\"/g;s/$/"/g;s/^/"/g'
 echo
 echo -n "Send these changes to redis? (y/n): "
 read N
