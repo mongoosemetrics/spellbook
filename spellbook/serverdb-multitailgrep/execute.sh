6ebb8dcbccd9dd167d4b3b91bcb652819abff3ab
Shea Rial <sheatrevor@gmail.com>
2013-07-19 15:23:48 -0700

    Well, this was working.

--- spellbook/serverdb-multitailgrep/execute.sh
+++ spellbook/serverdb-multitailgrep/execute.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-me=serverdb-list
-if [ $# != 2 ]; then
+me=serverdb-multitailgrep
+if [ $# != 5 ]; then
   spellbook help $me; exit 1
 fi
 category=$1
 environment=$2
-if [ ! -f ~/.serverdb ]; then
-  spellbook serverdb-reset
-fi
-sqlite3 ~/.serverdb "select hostname from servers where category='$category' and environment='$environment'"
+username=$3
+filename=$4
+search=$5
+sshpids=""
+remotecommand='"tail -f '$filename' | grep '$search'"'
+for hostname in `spellbook serverdb-list $category $environment`
+do
+  ssh "$username"@"$hostname" $remotecommand &
+  sshpids="$sshpids $!"
+done
+sleep 10
+kill $sshpids
