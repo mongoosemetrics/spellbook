67a39760b4e6d84378a3daebfd9296ae8eca4e4c
Shea Rial <sheatrevor@gmail.com>
2013-07-19 15:36:52 -0700

    Work.

--- spellbook/signer/execute.sh
+++ spellbook/signer/execute.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo $json | php signer.php $1
+me=signer
+if [# != 2 ]; then
+  spellbook help $me; exit 1
+fi
+hash=$1
+json=$2
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $json | php $mydir/signer.php $hash
